21e6cf7450b5834166df5fd15860e3c0e5353120
liuxiaolei <2453576273@qq.com>
2020-04-14 21:10:11 +0800

    Initial Commit

---
+++ pages/add/add.js
@@ -0,0 +1,23 @@
+Page({
+  data: {
+    date: '',
+    show: false
+  },
+
+  onDisplay() {
+    this.setData({ show: true });
+  },
+  onClose() {
+    this.setData({ show: false });
+  },
+  formatDate(date) {
+    date = new Date(date);
+    return `${date.getMonth() + 1}/${date.getDate()}`;
+  },
+  onConfirm(event) {
+    this.setData({
+      show: false,
+      date: this.formatDate(event.detail)
+    });
+  }
+});
