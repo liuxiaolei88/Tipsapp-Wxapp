24061e7cb4bcf1b76ad652fd4806bf0b0323d9dd
user <1476784946@qq.com>
2020-07-21 21:51:32 +0800

    1.重写了昨天上传丢失的全局样式 2.添加页面的部分 7.21

--- pages/add/add.js
+++ pages/add/add.js
@@ -1,23 +1,36 @@
 Page({
-  data: {
-    date: '',
-    show: false
+  data:{
+    show:false,
+    arr:[
+      {name:" 食品"},
+      {name:" 药品"},
+      {name:" 化妆品护肤品"}
+    ],
+    value:""
   },
-
-  onDisplay() {
-    this.setData({ show: true });
-  },
-  onClose() {
-    this.setData({ show: false });
-  },
-  formatDate(date) {
-    date = new Date(date);
-    return `${date.getMonth() + 1}/${date.getDate()}`;
-  },
-  onConfirm(event) {
+  onTap(){
     this.setData({
-      show: false,
-      date: this.formatDate(event.detail)
-    });
+      show:true
+    })
+
+  },
+  onClose(){
+    this.setData({
+      show:false
+    })
+  },
+  onSelect(res){
+    console.log(res.detail.name)
+    this.setData({
+      value:res.detail.name
+    })
+    
+
+  },
+  onClick(){
+    wx.showToast({
+      title: '成功',
+    })
+
   }
 });
