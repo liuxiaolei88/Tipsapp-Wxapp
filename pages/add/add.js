0d6fe13acbc2026d468fe81d8ac712fc361c9333
bear <2453576273@qq.com>
2023-04-21 15:13:39 +0800

    update：分一二张图自动填写

--- pages/add/add.js
+++ pages/add/add.js
@@ -1,13 +1,16 @@
+// 链接数据库
 const db = wx.cloud.database({
   env: 'tipsapp-4g4e3qbv29f41b1c'
 });
+// 链接表
 const todos = db.collection('userToken');
-var _sort='';
+var _sort = '';
 const app = getApp();
+
 Page({
   data: {
     show: false,
-    showhome:false,
+    showhome: false,
     arr: [{
         name: "食品"
       },
@@ -18,11 +21,11 @@ Page({
         name: "化妆品护肤品"
       }
     ],
-    arrhome:[{
+    arrhome: [{
       name: "选择空间"
     }],
-    value: "类别",
-    valuehome:"清单",
+    itemSort: "类别",
+    valuehome: "清单",
     date1: '',
     show1: false,
     date2: '',
@@ -37,13 +40,18 @@ Page({
     name: '',
     remark: '',
     count: '',
-    username: '',
-    number:0,
-    realptotopath:''
+    itemName: '', //物品名称
+    identifySort: '', //识别出来的物品类别
+    number: 0,
+    realptotopath: '',
+    // 预定义一些物品名称，目的是自动填写物品类别
+    foodItem: ['蛋糕', '咖啡', '巧克力', '面包', '果冻'],
+    makeUpItem: ['口红', '粉底液', '眼影'],
+    medicinalItem: ['风油精', '碘酒', '酒精', '三金 桂林西瓜霜喷剂 3.5g']
+
   },
-  onLoad: function (options) {
-  },
-  onShow:function(){
+  onLoad: function (options) {},
+  onShow: function () {
     this.getHomeList()
   },
   getHomeList() {
@@ -59,59 +67,178 @@ Page({
     ])).get({
       success: (res) => {
         let rest = []
-        for(let i of res.data){
+        for (let i of res.data) {
           rest.push({
-            name:i.homeid
+            name: i.homeid
           })
         }
         this.setData({
-          arrhome:rest
+          arrhome: rest
         })
       }
     })
   },
   afterRead: function (event) {
-    const {file} = event.detail;
-    const {fileList = []} = this.data;
-    fileList.push({url: file.path});
-    this.setData({fileList})
-
-
-    wx.getFileSystemManager().readFile({
-      filePath: this.data.fileList[0].url, //选择图片返回的相对路径
-      encoding: 'base64', //编码格式
-      complete: (res) => {
-        console.log(res)
-        console.log('comp')
-      },
-      success: (res) => {
-        console.log(res.data)
-      },
-      fail: (err) => {
-        console.log(err)
-      },
-      success: res => {
-        wx.request({
-          url: "https://aip.baidubce.com/rest/2.0/image-classify/v2/advanced_general?access_token=24.2f63c2c2a4286d7c14a8d8bc988462a1.2592000.1680336750.282335-21434170",
-          data: {
-            image: res.data,
-            image_type: "BASE64"
-          },
-          method: 'POST',
-          dataType: "json",
-          header: {
-            'Content-type': 'application/x-www-form-urlencoded'
-          },
-          success: res => {
-            console.log(res)
-            this.setData({
-              username: res.data.result[0].keyword
-            })
-
-          }
-        })
-      }
+    const {
+      file
+    } = event.detail;
+    const {
+      fileList = []
+    } = this.data;
+    fileList.push({
+      url: file.path
+    });
+    this.setData({
+      fileList
     })
+
+    // 照片1:对第一张照片进行主体识别
+    if (fileList.length === 1) {
+      console.log('第一张照片');
+      // 对第一张照片进行主体识别
+      wx.getFileSystemManager().readFile({
+        filePath: this.data.fileList[0].url,
+        // ocrFilePath: this.data.fileList[1].url,//返回传到ocr里的路径
+        encoding: 'base64', //编码格式
+
+        complete: (res) => {
+          console.log(res)
+          console.log('comp')
+        },
+        success: (res) => {
+          console.log(res.data)
+        },
+        fail: (err) => {
+          console.log(err)
+        },
+
+        // 请求图像识别
+        success: res => {
+          wx.request({
+            url: "https://aip.baidubce.com/rest/2.0/image-classify/v2/advanced_general?access_token=24.a8587cf1fbe9d34dbe721317b65d2311.2592000.1684552128.282335-21434170",
+            data: {
+              image: res.data,
+              image_type: "BASE64"
+            },
+            method: 'POST',
+            dataType: "json",
+            header: {
+              'Content-type': 'application/x-www-form-urlencoded'
+            },
+            // 回调成功以后
+            success: res => {
+              console.log(res)
+              var name = res.data.result[0].keyword
+              var sort = res.data.result[0].root.split('-')[1]
+              var remark = ''
+              // 判断预制识别的物品在不在预制类中，以自动填写类别
+              if (this.data.foodItem.indexOf(name) != -1) {
+                sort = '食物',
+                  remark = '要加速吃哦！'
+              } else if (this.data.makeUpItem.indexOf(name) != -1) {
+                sort = '化妆品护肤品',
+                  remark = '要加速使用哦！'
+              } else if (this.data.medicinalItem.indexOf(name) != -1) {
+                sort = '药品',
+                  remark = '要记得这里还有这个药物哦！'
+              }
+
+              this.setData({
+                itemName: name, //物品的名称
+                identifySort: sort, //模型识别的类别，还没有转换成自己的类别
+                itemSort: sort,
+                remark: remark,
+                number: 1 // 数量填写默认=1
+              })
+
+            }
+          })
+        }
+      })
+    }
+    // 照片2:对第二张照片进行OCR识别
+    else {
+      wx.getFileSystemManager().readFile({
+        filePath: this.data.fileList[1].url,
+        encoding: 'base64', //编码格式
+
+        complete: (res) => {
+          console.log(res)
+          console.log('comp')
+        },
+        success: (res) => {
+          console.log(res.data)
+        },
+        fail: (err) => {
+          console.log(err)
+        },
+
+        // 请求OCR识别
+        success: res => {
+          wx.request({
+            // url:'https://api.xf-yun.com/v1/private/sf8e6aca1',
+            url: "https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic?access_token=24.dd274e7ac70e470557632df41a591ea2.2592000.1684573066.282335-32621704",
+            data: {
+              image: res.data,
+              image_type: "BASE64"
+            },
+            method: 'POST',
+            dataType: "json",
+            header: {
+              'Content-type': 'application/x-www-form-urlencoded'
+            },
+            // 回调成功以后
+            success: res => {
+              console.log(res);
+              var ocrWordList = res.data.words_result
+              for (var item of ocrWordList) {
+                var itemWord = item['words']
+                // 判断识别的都是String，有些为undefined
+                if (typeof (itemWord) === 'string') {
+                  if (itemWord.indexOf('生产日期') !== -1) {
+                    console.log(itemWord);
+                    console.log(itemWord.split('：'));
+                    var birthDate = itemWord.split('：')[1]
+                    console.log(birthDate);
+                  }
+                  if (itemWord.indexOf('保质期') !== -1) {
+                    var outDate = itemWord.split('：')[1]
+                    console.log(outDate);
+                    // 判断是否是月份形式
+                    if (outDate.indexOf('个月') !== -1) {
+                      var monthNum = parseInt(outDate.split('个月'))
+                      console.log(monthNum );
+                      var year = monthNum / 12
+                      console.log(year);
+                      var birthDaterYear = parseInt(birthDate.split('/')[0])
+                      var outDateYear = birthDaterYear + year
+                      console.log(outDateYear);
+                      var outDateMonth = parseInt(birthDate.split('/')[1])
+                      console.log(outDateMonth);
+                      var outDateDay= parseInt(birthDate.split('/')[2])
+                      console.log(outDateDay);
+                      birthDate = birthDaterYear+'/'+outDateMonth+'/'+outDateDay
+                      var outDateFin = outDateYear+'/'+outDateMonth+'/'+outDateDay
+                      console.log(outDateFin);
+                    } else {
+                      console.log('');
+                    }
+                  }
+                }
+
+              }
+              this.setData({
+                date1: birthDate,
+                date2:outDateFin
+              })
+
+            }
+          })
+        }
+      })
+    }
+
+
   },
 
   formSubmit: function (data) {
@@ -132,14 +259,14 @@ Page({
     // event.detail 为当前输入的值
     // console.log(event.detail);
     this.setData({
-      remark:event.detail
+      remark: event.detail
     })
   },
   //获取物品数量信息
   onChangeCount(event) {
     var that = this
     this.setData({
-      number:event.detail
+      number: event.detail
     })
   },
 
@@ -166,10 +293,10 @@ Page({
     })
   },
   onSelect(res) {
-    _sort=res.detail.name
-    // console.log(res.detail.name)
+    _sort = res.detail.name
+    console.log(typeof (res.detail.name))
     this.setData({
-      value: res.detail.name
+      itemSort: res.detail.name
     })
 
 
@@ -181,13 +308,12 @@ Page({
   },
   onClick() {
     var that = this
-    console.log('触发click事件')
     wx.cloud.uploadFile({
       // cloudPath:'example.png',
-      cloudPath: "img/" + new Date().getTime()+'.png',
+      cloudPath: "img/" + new Date().getTime() + '.png',
       // filePath:'http://tmp/6xPefYkamSFA281c52ee03d14b17596ec13131b0d3a5.png'
       filePath: this.data.fileList[0].url, // 小程序临时文件路径
-         
+
     }).then(res => {
       console.log('触发回掉')
       console.log(res)
@@ -197,41 +323,41 @@ Page({
         count: this.data.number,
         data1: this.data.data1,
         // data2: this.data.data2,
-        name: this.data.username,
+        name: this.data.itemName,
         remark: this.data.remark,
         sort: _sort,
         photourl: res.fileID,
-        date1:this.data.date1,
-        date2:this.data.date2,
-        token:app.globalData.cloudID
-      } 
+        date1: this.data.date1,
+        date2: this.data.date2,
+        token: app.globalData.cloudID
+      }
       todos.add({
-        data:this.clouddata,
+        data: this.clouddata,
         // success: () => console.log("成功了"),
         error: res => console.log(res)
       })
-      var infoo=[]
+      var infoo = []
       db.collection('homelist').where({
-        homeid: this.data.valuehome
-      })
-      .get().then((res)=>{
-        infoo = res.data[0]
-        infoo.info.push(this.clouddata)
-        console.log(infoo)
-      })
-      .then(()=>{
-        db.collection('homelist').doc(infoo._id).update({
-          data:{
-            info:infoo.info
-          }
-        }).then((res)=>{
-          console.log(res)
+          homeid: this.data.valuehome
         })
-      })
-      
+        .get().then((res) => {
+          infoo = res.data[0]
+          infoo.info.push(this.clouddata)
+          console.log(infoo)
+        })
+        .then(() => {
+          db.collection('homelist').doc(infoo._id).update({
+            data: {
+              info: infoo.info
+            }
+          }).then((res) => {
+            console.log(res)
+          })
+        })
+
       this.setData({
-        value: "类别",
-        valuehome:"选择空间",
+        itemSort: "类别",
+        valuehome: "选择空间",
         date1: '',
         show1: false,
         date2: '',
@@ -246,13 +372,13 @@ Page({
         name: '',
         remark: '',
         count: '',
-        username: '',
-        number:0,
+        itemName: '',
+        number: 0,
       })
       wx.showToast({
         title: '提交啦!!',
       })
-  
+
 
     }).catch(error => {
       // handle error
@@ -261,67 +387,7 @@ Page({
       console.log(cloudPath)
       console.log(filePath)
     })
-    // this.clouddata = {
-    //   count: this.data.number,
-    //   data1: this.data.data1,
-    //   // data2: this.data.data2,
-    //   name: this.data.username,
-    //   remark: this.data.remark,
-    //   sort: _sort,
-    //   photourl: this.data.fileList[0].url,
-    //   date1:this.data.date1,
-    //   date2:this.data.date2,
-    //   token:app.globalData.cloudID
-    // }
-    // console.log(this.clouddata)
 
-    // todos.add({
-    //   data:this.clouddata,
-    //   // success: () => console.log("成功了"),
-    //   error: res => console.log(res)
-    // })
-    // var infoo=[]
-    // db.collection('homelist').where({
-    //   homeid: this.data.valuehome
-    // })
-    // .get().then((res)=>{
-    //   infoo = res.data[0]
-    //   infoo.info.push(this.clouddata)
-    //   console.log(infoo)
-    // })
-    // .then(()=>{
-    //   db.collection('homelist').doc(infoo._id).update({
-    //     data:{
-    //       info:infoo.info
-    //     }
-    //   }).then((res)=>{
-    //     console.log(res)
-    //   })
-    // })
-    
-    // this.setData({
-    //   value: "类别",
-    //   valuehome:"选择空间",
-    //   date1: '',
-    //   show1: false,
-    //   date2: '',
-    //   show2: false,
-    //   date3: '',
-    //   show3: false,
-    //   date4: '',
-    //   show4: false,
-    //   minDate: new Date(2019, 0, 1).getTime(),
-    //   maxDate: new Date(2050, 0, 31).getTime(),
-    //   fileList: [],
-    //   name: '',
-    //   remark: '',
-    //   count: '',
-    //   username: '',
-    //   number:0,
-    // })
-    // wx.showToast({
-    //   title: '提交啦！',
-    // })
 
   },
 
---
+++ req.md
@@ -0,0 +1,19 @@
+请求接口获取token
+图像：
+API Key：ziOE0hLn1tFGlcwkceZnn9LI
+Secret Key：VALm6bQxEIEsyh9fUG1sifOf5pABaj82
+"access_token":"24.a8587cf1fbe9d34dbe721317b65d2311.2592000.1684552128.282335-21434170"
+curl -i -k 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=ziOE0hLn1tFGlcwkceZnn9LI&client_secret=VALm6bQxEIEsyh9fUG1sifOf5pABaj82'
+
+
+ocr
+API Key：GxeNKVw1SbnSrl2VMmb4Fa82
+Secret Key：4axcoGFWgfxFs2L2cuXf83PZpLSMvczh
+
+curl -i -k 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=GxeNKVw1SbnSrl2VMmb4Fa82&client_secret=4axcoGFWgfxFs2L2cuXf83PZpLSMvczh'
+
+"access_token":"24.dd274e7ac70e470557632df41a591ea2.2592000.1684573066.282335-32621704"
+
+https://aip.baidubce.com/rest/2.0/ocr/v1/general?access_token=24.dd274e7ac70e470557632df41a591ea2.2592000.1684573066.282335-32621704
+
+
